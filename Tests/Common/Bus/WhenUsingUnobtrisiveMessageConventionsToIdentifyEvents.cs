f23747bee9303c97eb4db8a828facd7563517841
Dave Rael <dave@raelyard.com>
2014-10-02 14:28:59 -0400

    Added Unobtrusive message conventions class
    to prepare for identifying types for commands and events

---
+++ Tests/Common/Bus/WhenUsingUnobtrisiveMessageConventionsToIdentifyEvents.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MediaLoanLIbrary.Fines.PublicEvents;
+using NUnit.Framework;
+using Should;
+
+namespace MediaLoanLIbrary.Fines.Tests.Common.Bus
+{
+    [TestFixture]
+    public class WhenUsingUnobtrisiveMessageConventionsToIdentifyEvents
+    {
+        [Test]
+        public void ShouldMatchOnPublicEventType()
+        {
+            UnobtrusiveMessageConventions.EventsDefinition(typeof(SomethingHappendEvent)).ShouldBeTrue();
+        }
+    }
+
+    public class UnobtrusiveMessageConventions
+    {
+        public static Func<Type, bool> EventsDefinition
+        {
+            get { return type => true; }
+        }
+    }
+}
+
+namespace MediaLoanLIbrary.Fines.PublicEvents
+{
+    public interface SomethingHappendEvent
+    {
+    }
+}
